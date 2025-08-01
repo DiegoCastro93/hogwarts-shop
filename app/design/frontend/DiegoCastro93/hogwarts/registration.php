e29f63eeacea9fb23669cbdb3e972891304b4c0d
DiegoCastro93 <diego93cr@hotmail.com>
2025-07-31 22:44:04 -0500

    Custom theme: Create the base for the Hogwarts theme.

---
+++ app/design/frontend/DiegoCastro93/hogwarts/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright [first year code created] Adobe
+ * All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/DiegoCastro93/hogwarts', __DIR__);
